f5a24534df90d47cf22309f54876fb1e2448136e
therealmai <jomar35leano@gmail.com>
2022-03-07 20:12:25 +0800

    Finished

--- Leano_Calculator/Leano_Calculator/MainPage.xaml.cs
+++ Leano_Calculator/Leano_Calculator/MainPage.xaml.cs
@@ -15,17 +15,111 @@ namespace Leano_Calculator
             InitializeComponent();
         }
 
+        private decimal number1;
+        private string operatorName;
+        private bool isClickedOperator;
+        private bool isClickedDecimal = false;
+
         private void btnNumber_Clicked(object sender, EventArgs e)
         {
             var button = sender as Button;
-            if(LabelResult.Text == "0")
+            if(LabelResult.Text == "0" || isClickedOperator)
             {
+                isClickedOperator = false;
                 LabelResult.Text = button.Text;
             }
             else
             {
                 LabelResult.Text += button.Text;
             }
+        }
+
+        private void resetButton_Clicked(object sender, EventArgs e)
+        {
+            LabelResult.Text = "0";
+            isClickedOperator = false;
+            isClickedDecimal = false;
+            number1 = 0;
+        }
+
+        private void deleteButton_Clicked(object sender, EventArgs e)
+        {
+            string number = LabelResult.Text;
+            char decimalPoint = number[number.Length-1];
+            if(number != "0")
+            {
+                if(decimalPoint == '.')
+                {
+                   isClickedDecimal = false;
+                }
+                number = number.Remove(number.Length - 1, 1);
+                if (string.IsNullOrEmpty(number))
+                {
+                    LabelResult.Text = "0";
+                }
+                else
+                {
+                    LabelResult.Text = number;
+                }
+            }
+        }
+
+        private void operationButton_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            isClickedOperator = true;
+            operatorName = button.Text;
+            isClickedDecimal = false;
+            try
+            {
+                number1 = Convert.ToDecimal(LabelResult.Text);
+            }catch
+            {
+                DisplayAlert("Error", "Syntax Error", "Okay");
+            }
+        }
+
+        private void equalButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal number2 = Convert.ToDecimal(LabelResult.Text);
+                string finalResult = calcuResult(number1, number2).ToString("0.##");
+                LabelResult.Text = finalResult;
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Okay");
+            }
+          
+        }
+
+        public decimal calcuResult(decimal number1, decimal number2)
+        {
+            decimal result = 0;
+            switch (operatorName)
+            {
+                case "+": result = number1 + number2; break;
+                case "-": result = number1 - number2; break;
+                case "x": result = number1 * number2; break;
+                case "/": result = number1 / number2; break;
+            }
+            return result;
+        }
+
+        private void decimalButton_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (isClickedDecimal)
+            {
+                DisplayAlert("Error", "Can't add another Decimal Point", "Okay");
+            }
+            else
+            {
+                LabelResult.Text += button.Text;
+                isClickedDecimal = true;
+            }
+
         }
     }
 }
